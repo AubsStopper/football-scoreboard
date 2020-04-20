5611be5daacfcb77d0322f6ea022a81dff57d810
Aubrey Sledzinski <asledzinski88@gmail.com>
2020-04-19 23:23:34 -0400

    Feat: Created Scoreboard Component
    created scoreboard component and prop drilled home and away scores
    from app to scoreboard.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import BottomRow from "./BottomRow";
+import Scoreboard from "./components/scoreboard";
 import "./App.css";
 
 function App() {
@@ -31,22 +32,7 @@ function App() {
 
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
+      <Scoreboard apple={homeScore} orange={awayScore} />
       <section className="buttons">
         <div className="homeButtons">
 
